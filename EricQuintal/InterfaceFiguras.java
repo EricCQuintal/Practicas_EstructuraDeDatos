d0b0e8af1627c9f8af7f7718b1014d9415cb0c25
Eric Quintal <canulquintalerik@gmail.com>
2021-09-17 19:54:37 -0500

    .

--- EricQuintal/InterfaceFiguras.java
+++
@@ -1,9 +0,0 @@
-package EricQuintal;
-//se genera Interface con metodos abstractos estos metodos seran exportados 
-//en todas las clases que corresponden a los objetos o clases de los objetos
-interface InterfaceFiguras {
-
-    public void CalcularArea(double area);
-    public void CalcularVolumen(double volumen);
-    
-}
