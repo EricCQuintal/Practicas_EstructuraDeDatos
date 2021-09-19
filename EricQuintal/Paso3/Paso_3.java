7d714ce8724c576ac3ee91cfce7c6bcda4dc1f7a
Eric Quintal <canulquintalerik@gmail.com>
2021-09-18 21:18:15 -0500

    Explicacion de funcionamiento  de paso 3 y 4

--- EricQuintal/Paso3/Paso_3.java
+++ EricQuintal/Paso3/Paso_3.java
@@ -1,35 +1,63 @@
 package EricQuintal.Paso3;
-
+//se ingresan las siguientes librerias para el uso correcto y funcionamiento del
+//programa 
 import java.util.ArrayList;
-
 import javax.swing.JOptionPane;
 
 public class Paso_3 {
  public static void main(String[] args) {
+     //se agrega el array list o arreglo con la variable de nombre listado
      ArrayList<String> listado;
-     listado=new ArrayList<String>(); 
+     //se instancia un nuevo objeto llamado listado 
+     listado=new ArrayList<String>();
+     //se asigna las variables de tipo int y String 
      String tempo, resp;
      int n;
+     //se inicializa una estrcutura do while con la cual tendra una funcion de
+     //recibir los datos proporcionados por los usuarios y agregar mas 
+     //palabras con la opcion de s/n  
      do {
+         //manda un mensaje pidiendole al usuario agregue una nueva palabra
          tempo=JOptionPane.showInputDialog(null, "Escribe una palabra: ");
+         //este es asignado al arreglo listado y Agregado ala variable tempo
          listado.add(tempo);
          resp=JOptionPane.showInputDialog(null, "¿Desea ingresar otra palabra? s/n: ");
+         //se le asigna el valor a la variable resp 
+         //El metodo toLowerCase sirve para devorlver e valor a la cadena en 
+         //minusculas 
          resp=resp.toLowerCase();
 
-         
+
+       //el while usa una condicional la cual se repetira si la respuesta 
+       //es diferente a n esta seguira pidiendo los datos para que siga 
+       //porporcionando el usuario nuevos datos  
      } while (!resp.equals("n"));
-
+    //este metodo mostrara el listado actual despues de capturar las primeras
+    //palabras porporcionadas por el usuario
     MostrarListado(listado);
+    //este metodo servira para modificar la lista
+    //por consiguiente nos saldra un mensaje por pantalla pidiento que elijas
+    //un dato a modificar se aignara en la variable n de tipo int
     n= Integer.parseInt(JOptionPane.showInputDialog(null, "¿Que elemento vas a modificar de la lista?\n Elije un numero de la lista: "));
+    //esta linea de codigo pedira los datos nuevos proporcionados por el usuario
+    //y agregara la nueva palabra en el anterior lugar de la palabra modificada
     listado.set((n-1),tempo=JOptionPane.showInputDialog(null, "Escribe nueva palabra: "));
-
+     
+    //motrara el listado nuevo ya modificado 
     MostrarListado(listado);
+    //esta se asiganara el numero a capturar para borrar el valor que el usuario
+    //porporcione y eliminara segun la posicion y numero de la lista
     n= Integer.parseInt(JOptionPane.showInputDialog(null, "¿Que elemento vas a eliminar de la lista?\n Elije un numero de la lista: "));
     listado.remove((n-1));
+
+    //mostrara el los datos actualizados eliminado el dato seleccionado por el 
+    //usuario
     MostrarListado(listado);
     
  }
-
+//este metodo asiganar los valores segun corresponda los pasos realizados
+//anteriormente e impirmira las capturas del usuario, modificaicones y 
+//eliminaciones de datos
 private static void MostrarListado(ArrayList<String> listado) {
 System.out.println("\nElementos de la lista");
 for (int i=0; i<listado.size(); i++) {
--- EricQuintal/practica2/nElementos.java
+++ EricQuintal/practica2/nElementos.java
@@ -1,22 +1,34 @@
 package EricQuintal.practica2;
-
+//se paso a implemetar las siguientes librerias 
 import java.util.Scanner;
 import java.lang.Integer;
 import javax.swing.JOptionPane;
-
+//la clase fue llamada nElementos haciendo referencia que el programa ya que este
+//pide al usuario que ingrese n elemntos al listado es decir no tiene limites
+//al pedir datos al usuario
 public class nElementos {
     public static void main(String[] args) {
         System.out.println("...................................................... ");
         System.out.println("PROGRAMA TOMA DATOS DE UN ARREGLO Y DEVUELVE LA SUMA");
         System.out.println("...................................................... ");
         Scanner sc=new Scanner(System.in);
+        //se paso a implementar los siguientes atributos
         int nElementos;
         int suma=0;
-        
+        //se inicializa la variable nElementos para que se asigne los datos 
+        //a recibir 
         nElementos=Integer.parseInt(JOptionPane.showInputDialog("Dijite la cantidad de elementos del arreglo: "));
+        //se instancia el objeto numeros y se asigna a el array nElementos 
         int [] numeros=new int [nElementos];
-
+        //se manda un mensaje por consola pideindo al usuario proporcione
+        //por medio del teclado el tamaño de los elementos del arreglo
         System.out.println("Digite los elementos del arreglo: ");
+        //se porcede a iniciar un bucle for en donde obtendrmos los datos 
+        //porporcionados por el usuario ya que se asignara el valor 
+        //que el usuario ingrese por medio del teclado con la condicion
+        //de que este inicie desde cero y el iterador sea menor ala
+        // variable nElementos ya que esta finalizaria si es mayor
+        //y aumentaria de 1 en 1
         for (int i=0; i<nElementos; i++) {
             System.out.print((i+1)+".Ingrese un numero: ");
             numeros[i]=sc.nextInt();
@@ -24,19 +36,26 @@ public class nElementos {
 
             
         } 
+        //Este metodo nos proporcionara y mostrara los datos ya proporcionado 
+        //por el usuario 
         System.out.println("............................................. ");
         System.out.println("Los elementos del arreglo son: ");
         System.out.println("............................................. ");
+        //de la misma maneraa se iniciara un bucle for y este nos proporcionara
+        //las mismas concdiciones con la diferencia que este nos mostrara
+        //el listado con los datos de los elementos que disponemos
         for (int i=0; i<nElementos; i++){
-            System.out.println((i+1)+"."+numeros[i]);
+            System.out.println((i+1)+". "+numeros[i]);
         }
-        
+        //Este metodo nos proporcionara las mismas conndiciones a diferencia
+        //que este sumara el valor de los datos asignados
         System.out.println("............................................. ");
         for (int i=0; i<nElementos; i++){
            suma +=numeros[i];
            
             
         }
+        //este es un mensaje que nos proporcionara el valor de la suma
         System.out.println("La suma de los elementos es: "+suma);
 
         
