b453cf9e0b97dc01cc81fb743c682ef790e3e480
Eric Quintal <canulquintalerik@gmail.com>
2021-11-03 18:59:09 -0600

    hola

---
+++ EricQuintal/Practica_6/NodoR.java
@@ -0,0 +1,20 @@
+package EricQuintal.Practica_6;
+
+public class NodoR {
+    int dato;
+    String nombre;
+    NodoR hDer, hIsq;
+    public NodoR(int d, String nom){
+        this.dato=d;
+        this.nombre=nom;
+        this.hDer=null;
+        this.hIsq=null;
+
+
+    }
+    public String toString(){
+        return dato +" --> " + nombre;
+        
+    }
+
+}
