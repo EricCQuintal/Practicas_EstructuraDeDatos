3840bb9d5a0c2691cc1a3759c4622f46973575b6
Eric Quintal <canulquintalerik@gmail.com>
2021-11-25 00:33:50 -0600

    Ismael ya te lo modifique checa wee

--- Practica_8/Mezcla_Directa/MezclaMain.java
+++ Practica_8/Mezcla_Directa/MezclaMain.java
@@ -1,10 +1,30 @@
 package Practica_8.Mezcla_Directa;
 
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class MezclaMain {
     public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+        //se paso a implementar los siguientes atributos
+        int nElementos;
+        nElementos=Integer.parseInt(JOptionPane.showInputDialog("Dijite la cantidad de elementos del arreglo: "));
+
+        int [] vector2=new int [nElementos];
+
+        System.out.println("Digite los elementos del arreglo: ");
+
+        for (int i=0; i<nElementos; i++) 
+        {
+            System.out.print((i+1)+".Ingrese un numero: ");
+            vector2[i]=sc.nextInt();
+
+        } 
+		
 		Mezcla_Directa Ordenar= new Mezcla_Directa ();
-		int vector1[]= {5,6,3,44,22,1};
-		int vector2[]= {5,2,1,8,3,9,7,44,3,5,2};
+		
 		System.out.println("Vector 2 original");
 		Ordenar.mostrarArreglo(vector2);
 		System.out.println("Vector 2 ordenado");
