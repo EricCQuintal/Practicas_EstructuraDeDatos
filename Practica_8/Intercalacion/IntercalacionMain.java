99975be682fa0d944c73f7e28b7826f7ca817ea8
Eric Quintal <canulquintalerik@gmail.com>
2021-11-25 00:26:15 -0600

    ya agregue el codigo que pide ambos vectores

--- Practica_8/Intercalacion/IntercalacionMain.java
+++ Practica_8/Intercalacion/IntercalacionMain.java
@@ -1,10 +1,47 @@
 package Practica_8.Intercalacion;
 
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class IntercalacionMain {
     public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        //se paso a implementar los siguientes atributos
+        int nElementos;
+        nElementos=Integer.parseInt(JOptionPane.showInputDialog("Dijite la cantidad de elementos del arreglo: "));
+
+        int [] vector1=new int [nElementos];
+
+        System.out.println("Digite los elementos del arreglo: ");
+
+        for (int i=0; i<nElementos; i++) 
+        {
+            System.out.print((i+1)+".Ingrese un numero: ");
+            vector1[i]=sc.nextInt();
+
+        } 
+
+		Scanner teclado=new Scanner(System.in);
+        //se paso a implementar los siguientes atributos
+        int nElementos2;
+        nElementos2=Integer.parseInt(JOptionPane.showInputDialog("Dijite la cantidad de elementos del arreglo: "));
+
+        int [] vector2=new int [nElementos2];
+
+        System.out.println("Digite los elementos del arreglo: ");
+
+        for (int i=0; i<nElementos2; i++) 
+        {
+            System.out.print((i+1)+".Ingrese un numero: ");
+            vector2[i]=teclado.nextInt();
+
+        } 
+
+		
+		
 		Intercalacion Ordenar= new Intercalacion();
-		int vector1[]= {5,6,3,44,22,1};
-		int vector2[]= {5,2,1,8,3,9,7,44,3,5,2};
+		
 		System.out.println("Arreglo vector 1 original");
 Ordenar.mostrarArreglo(vector1);
 System.out.println("Arreglo vector 2 original");
